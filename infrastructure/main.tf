f5ce1d7202984c121c62d2a2177db00978fae63a
Thibaut-T <thibauttournemaine@gmail.com>
2024-11-20 16:07:03 +0100

    feat(infrastructure): Adding functions to terraform in order to deploy with Azure

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -13,6 +13,46 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "casper" {
-  name     = "Casper"
+  name     = "rg${random_integer.storage_name.id}"
   location = "francecentral"
 }
+
+
+resource "random_integer" "storage_name" {
+  min = 0
+  max = 10000
+}
+
+
+resource "azurerm_storage_account" "storacc" {
+  name                     = "storacc${random_integer.storage_name.id}"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+
+resource "azurerm_service_plan" "example" {
+  name                = "serviceplan${random_integer.storage_name.id}" //Change this for more clarity 
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  os_type             = "Windows"
+  sku {
+    tier = "Basic"     #Double check this
+    size = "B1"   
+  }
+}
+
+
+resource "azurerm_windows_web_app" "example" {
+  name                = "webapp${random_integer.storage_name.id}" 
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_service_plan.example.location
+  service_plan_id     = azurerm_service_plan.example.id
+
+  //site_config {}    Maybe activate this in order to complete the deploy 
+}
+
+
+
